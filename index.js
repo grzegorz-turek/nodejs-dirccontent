956810b4e37e6110bbd11ac782b86e5788b6408f
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-06-16 23:01:24 +0200

    Final v.6

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var fs = require('fs');
 
 fs.readdir('./', function(err, files) {
     if (err) {
-        console.log('Fatal error. Can\'t read dir. Check possible reasons.')
+        console.log('Fatal error. Can\'t read dir. Check possible reasons.');
         process.exit();
-    };
+    }
     fs.writeFile('./content.txt', files, function(err) {
         if (err) {
-            console.log('Fatal error. Can\'t write file. Check if file or dir are not read-only')
-            process.exit();    
+            console.log('Fatal error. Can\'t write file. Check if file or dir are not read-only');
+            process.exit();
         }
         console.log('Saved to ./content.txt!');
     });
